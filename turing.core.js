60d76b45621be2c6f53ef157cc82dd7a15d93fa0
John Wu <webmaster@leapoahead.com>
2014-02-08 00:52:10 +0800

    bug fix

--- turing.core.js
+++ turing.core.js
@@ -7,24 +7,21 @@
 
   /**
    * AMD模块载入
-   * @param  {string}   module
-   * @param  {array}   dependencies
-   * @param  {Function} fn
-   * @return {module} 返回的模块
+   * @param  {string}   moduleName 模块名称
+   * @param  {array}   dependencies 依赖模块列表
+   * @param  {Function} fn 模块主体
    */
-  turing.define = function(module, dependencies, fn) {
+  turing.define = function(moduleName, dependencies, fn) {
     if (!! global.define) {
-      return global.define(module, dependencies, fn);
+      global.define(moduleName, dependencies, fn);
     }
     else {
       var injectedDependencies = [], i;
       for (i = 0; i < dependencies.length; i ++) {
-        if (! turing._modules[dependencies[i]]) {
-          throw new Error('turing: dependency does not exists');
-        }
         injectedDependencies.push(turing._modules[dependencies[i]]);
       }
-      fn.apply(undefined, injectedDependencies);
+      // 任何模块执行时的上下文应该为undefined
+      turing._modules[moduleName] = fn.apply(undefined, injectedDependencies); 
     }
   }
 
