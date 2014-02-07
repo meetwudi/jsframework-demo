3dcf323f2cc15f01be12e9d6a3e9635b621cd53a
John Wu <webmaster@leapoahead.com>
2014-02-07 23:13:30 +0800

    jsdoc & turing.define

--- turing.core.js
+++ turing.core.js
@@ -1,8 +1,32 @@
 (function(global){
   var turing = {
-    VERSION: '0.0.1', 
-    lesson: 'Part 1: Library Architecture'
+    VERSION: '0.0.0'
   };
+
+  turing._modules = {};
+
+  /**
+   * AMD模块载入
+   * @param  {string}   module
+   * @param  {array}   dependencies
+   * @param  {Function} fn
+   * @return {module} 返回的模块
+   */
+  turing.define = function(module, dependencies, fn) {
+    if (!! global.define) {
+      return global.define(module, dependencies, fn);
+    }
+    else {
+      var injectedDependencies = [], i;
+      for (i = 0; i < dependencies.length; i ++) {
+        if (! turing._modules[dependencies[i]]) {
+          throw new Error('turing: dependency does not exists');
+        }
+        injectedDependencies.push(turing._modules[dependencies[i]]);
+      }
+      fn.apply(undefined, injectedDependencies);
+    }
+  }
 
   if (global.turing) {
     throw new Error('turing: top-level turing object already exists!');
