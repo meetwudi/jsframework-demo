1b5df0177245d21cac47199509637acb384e2208
John Wu <webmaster@leapoahead.com>
2014-02-08 01:00:20 +0800

    jsdoc

--- build/turing.js
+++ build/turing.js
@@ -1,1 +1,35 @@
-!function(e){var n={VERSION:"0.0.0"};if(n._modules={},n.define=function(i,r,o){if(e.define)return e.define(i,r,o);var t,d=[];for(t=0;t<r.length;t++){if(!n._modules[r[t]])throw new Error("turing: dependency does not exists");d.push(n._modules[r[t]])}o.apply(void 0,d)},e.turing)throw new Error("turing: top-level turing object already exists!");e.turing=n}("undefined"==typeof window?this:window);
+(function(global){
+  var turing = {
+    VERSION: '0.0.0'
+  };
+
+  turing._modules = {};
+
+  /**
+   * AMD模块载入
+   * @param  {string}   moduleName 模块名称
+   * @param  {array}   dependencies 依赖模块列表
+   * @param  {Function} fn 模块主体
+   */
+  turing.define = function(moduleName, dependencies, fn) {
+    if (!! global.define) {
+      global.define(moduleName, dependencies, fn);
+    }
+    else {
+      var injectedDependencies = [], i;
+      for (i = 0; i < dependencies.length; i ++) {
+        injectedDependencies.push(turing._modules[dependencies[i]]);
+      }
+      // 任何模块执行时的上下文应该为undefined
+      turing._modules[moduleName] = fn.apply(undefined, injectedDependencies); 
+    }
+  }
+
+  if (global.turing) {
+    throw new Error('turing: top-level turing object already exists!');
+  }
+  else {
+    global.turing = turing;
+  }
+
+})(typeof window === 'undefined' ? this : window);
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    jsdoc = require('gulp-jsdoc'),
+    rename = require('gulp-rename');
 
 gulp.task('build', function() {
   return gulp.src(['turing.core.js'])
           .pipe(concat('turing.js'))
+          .pipe(gulp.dest('build'))
+          .pipe(rename({suffix:".min"}))
           .pipe(uglify({outSourceMap: true}))
           .pipe(gulp.dest('build'));
 });
@@ -17,6 +21,11 @@ gulp.task('test', function() {
 
 });
 
+gulp.task('doc', function() {
+  gulp.src('build/turing.js')
+    .pipe(jsdoc('./doc'));
+});
+
 gulp.task('default', function() { 
-  gulp.start('build');
+  gulp.start('lint', 'test', 'build', 'doc');
 });
