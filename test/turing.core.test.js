846d75848dfed96759df73d422a905bfbdcb3bbf
John Wu <webmaster@leapoahead.com>
2014-02-08 14:51:12 +0800

    add test cases

--- test/turing.core.test.js
+++ test/turing.core.test.js
@@ -1,5 +1,13 @@
 describe('turing.core', function() {
   it('should be able to use define to load AMD-Style module', function() {
-    
+    turing.define('test-module-first', [], function() {
+      return {
+        value: 1
+      };
+    });
+
+    turing.define('test-module-second', ['test-module-first'], function(testModuleFirst) {
+      testModuleFirst.value.should.eql(1);
+    });
   });
 });
