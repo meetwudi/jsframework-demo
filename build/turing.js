87ca4ddbdcef49298492d4b46e35b85510133ead
John Wu <webmaster@leapoahead.com>
2014-02-08 21:28:15 +0800

    regular commit

--- build/turing.js
+++ build/turing.js
@@ -63,4 +63,20 @@ define('turing', ['global'], function(global) {
  */
 
 define('turing.oo', ['turing'], function(turing) {
+  /**
+   * 创建新类
+   * 
+   * @method  turing.createClass
+   * @return {Class} 
+   * @example
+   *      var Dog = turing.createClass({
+   *        inherits: Animal, // 继承类
+   *        body: {           // 实例属性与方法
+   *          
+   *        } 
+   *      });
+   */
+  turing.createClass = function(options) {
+    
+  };
 });
--- turing.oo.js
+++ turing.oo.js
@@ -5,4 +5,20 @@
  */
 
 define('turing.oo', ['turing'], function(turing) {
+  /**
+   * 创建新类
+   * 
+   * @method  turing.createClass
+   * @return {Class} 
+   * @example
+   *      var Dog = turing.createClass({
+   *        inherits: Animal, // 继承类
+   *        body: {           // 实例属性与方法
+   *          
+   *        } 
+   *      });
+   */
+  turing.createClass = function(options) {
+    
+  };
 });
